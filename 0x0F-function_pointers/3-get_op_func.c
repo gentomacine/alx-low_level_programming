24aefa70990dec2d97d996e97f0db1408bcf0583
gentomacine <ogwaluchidozie@gmail.com>
2022-04-13 13:53:33 +0100

    first

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,33 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * get_op_func - selects the correct function to perform
- * the operation asked by the user.
- * @s: char operator.
- *
- * Return: pointer to the function that corresponds to the operator.
- */
+  * get_op_func - choosing the right func for the operator
+  * @s: the operator
+  *
+  * Return: result of function or error
+  */
 int (*get_op_func(char *s))(int, int)
 {
+	/* declarations */
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
-	int i = 0;
-
-	while (i < 10)
+	int i;
+	/* matches operator to function */
+	i = 0;
+	while (ops[i].op)
 	{
-		if (s[0] == ops->op[i])
-			break;
+		if (ops[i].op[0] == *s && s[1] == '\0')
+			return (ops[i].f);
 		i++;
 	}
-
-	return (ops[i / 2].f);
+	/* bad operator, print error */
+	printf("Error\n");
+	exit(99);
 }
