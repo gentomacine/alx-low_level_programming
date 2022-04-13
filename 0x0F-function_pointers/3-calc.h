b288589d5b614f164b2fba91b7731ddb0bf91f0a
gentomacine <ogwaluchidozie@gmail.com>
2022-04-13 16:00:51 +0100

    first

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,8 @@
 #ifndef _CALC_H_
 #define _CALC_H_
 
-
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct op - Struct op
  *
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,40 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
-  * main - entry point
-  * @argc: # of args
-  * @argv: array of pointers to names of args
-  * Return: 0
-  */
-int main(int argc, char **argv)
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	/* input checking */
+	int a, b;
+	int (*operation)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+
+	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	/* calling get_op_func with arguments */
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
 	return (0);
 }
