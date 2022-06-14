a70e7d04a24b210a78177e3d4f8d7cd9f4dc6a09
gentomacine <ogwaluchidozie@gmail.com>
2022-06-14 05:22:46 +0100

    irhf

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/gentomacine/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://raw.github.com/gentomacine/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
